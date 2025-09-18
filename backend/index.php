d9c1b42a3275b09d75fcd4cc8ba95600a98f2300
jennica <jennica3003@gmail.com>
2025-09-18 16:36:16 +0800

    additional

--- backend/index.php
+++ backend/index.php
@@ -1,1 +1,30 @@
-<?php echo 'Hello Backend'; ?>
+   <?php
+     echo "Hello PHP! <br>";
+     echo "Hello PHP! <br>";
+     echo "Hello " . "PHP" . '! <br>';
+    ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+     echo "Hello PHP! <br>";
+     echo "Hello PHP! <br>";
+     echo "Hello " . "PHP" . '! <br>';
+    ?>
+
+</body>
+</html>
+
+   <?php
+     echo "Hello PHP! <br>";
+     echo "Hello PHP! <br>";
+     echo "Hello " . "PHP" . '!';
+    ?>
