cb934c52d3d553d76ae8794d1891c36f3af97845
jennica <jennica3003@gmail.com>
2025-09-19 16:02:06 +0800

    new added

---
+++ backend/06_loops.php
@@ -0,0 +1,23 @@
+<?php
+
+/*For Loops
+for($i = 1; $i <= 5; $i++){
+    echo $i . " ";
+}
+*/
+
+/* While loop
+$i = 1;
+while($i <= 5){
+    echo $i . " ";
+    $i++;
+}
+*/
+
+// foreach Loop
+$fruits = ["apple", "banana", "cherry"];
+foreach($fruits as $fruits){
+    echo $fruits . " ";
+}
+
+?>
